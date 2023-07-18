1933e9bd0162ecc61763cc60242977c8aeec56a1
Jaeyeong Hwang <99489807+jwyeeh-dev@users.noreply.github.com>
2023-07-18 17:26:56 +0900

    add sourcePID codes in board

--- ControlParts/ArduinoPart/sourcePID.ino
+++ ControlParts/ArduinoPart/sourcePID.ino
@@ -1,22 +1,57 @@
-#include <PID_v1.h>
+#include <math.h>
 
-#define leftEncoderPinA 3
-#define leftEncoderPinB 4
-#define rightEncoderPinA 8
-#define rightEncoderPinB 9
-#define leftMotorPin1 5
-#define leftMotorPin2 6
-#define rightMotorPin1 10
-#define rightMotorPin2 11
+#define PI 3.14159265
 
-double Setpoint, Input, Output;
-double Kp=2, Ki=5, Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+// Motor Pin Configuration
+#define leftMotorPin1 11
+#define leftMotorPin2 10
+#define rightMotorPin1 6
+#define rightMotorPin2 5
+#define leftMotorPinPWM 12  // Left Motor PWM Pin
+#define rightMotorPinPWM 7  // Right Motor PWM Pin
+
+// Encoder Pin Configuration
+#define leftEncoderPinA 8
+#define leftEncoderPinB 9
+#define rightEncoderPinA 3
+#define rightEncoderPinB 4
+
+// PID Constants
+#define Kp 1.0 // Proportional control gain
+#define Ki 0.5 // Integral control gain
+#define Kd 0.1 // Derivative control gain
 
 volatile int leftEncoderPos = 0;
 volatile int rightEncoderPos = 0;
+int prevLeftEncoderPos = 0;
+int prevRightEncoderPos = 0;
+
+// Robot specifications
+const double gearboxRatio = 27.0;
+const double encoderPulsesPerRevolution = 26.0;
+const double wheelDiameter = 13.0; // cm
+const double wheelbase = 30.0; // cm
+const double wheelCircumference = wheelDiameter * PI;
+const double distancePerPulse = wheelCircumference / (encoderPulsesPerRevolution * gearboxRatio); // cm/pulse
+
+// PID Variables
+double leftSpeedSetpoint = 0.0; // Desired left wheel speed
+double rightSpeedSetpoint = 0.0; // Desired right wheel speed
+double leftSpeedError = 0.0; // Error in left wheel speed
+double rightSpeedError = 0.0; // Error in right wheel speed
+double leftSpeedErrorSum = 0.0; // Sum of left wheel speed errors
+double rightSpeedErrorSum = 0.0; // Sum of right wheel speed errors
+double prevLeftSpeedError = 0.0; // Previous error in left wheel speed
+double prevRightSpeedError = 0.0; // Previous error in right wheel speed
+
+// Robot's position and orientation (initially 0)
+double xPosition = 0.0;
+double yPosition = 0.0;
+double theta = 0.0;
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(leftEncoderPinA, INPUT);
   pinMode(leftEncoderPinB, INPUT);
   pinMode(rightEncoderPinA, INPUT);
@@ -25,66 +60,117 @@ void setup() {
   pinMode(leftMotorPin2, OUTPUT);
   pinMode(rightMotorPin1, OUTPUT);
   pinMode(rightMotorPin2, OUTPUT);
+  pinMode(leftMotorPinPWM, OUTPUT);
+  pinMode(rightMotorPinPWM, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(leftEncoderPinA), updateLeftEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(leftEncoderPinB), updateLeftEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(rightEncoderPinA), updateRightEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(rightEncoderPinB), updateRightEncoder, CHANGE);
-  
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(10);
-  
-  Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available()){
-    Setpoint = Serial.parseInt(); //get the target speed from serial monitor
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    switch(command) {
+      case 'F': // Forward
+        leftSpeedSetpoint = rightSpeedSetpoint = 200; // Desired speed for forward
+        break;
+      case 'B': // Backward
+        leftSpeedSetpoint = rightSpeedSetpoint = -200; // Desired speed for backward
+        break;
+      case 'L': // Turn left
+        leftSpeedSetpoint = -100; // Desired speed for left turn
+        rightSpeedSetpoint = 100; // Desired speed for left turn
+        break;
+      case 'R': // Turn right
+        leftSpeedSetpoint = 100; // Desired speed for right turn
+        rightSpeedSetpoint = -100; // Desired speed for right turn
+        break;
+    }
   }
 
-  Input = (leftEncoderPos + rightEncoderPos) / 2.0; // take the average encoder value
+  // Compute desired speed control values using PID
+  double leftControl = computePIDControl(leftSpeedSetpoint, leftSpeedError, leftSpeedErrorSum, prevLeftSpeedError);
+  double rightControl = computePIDControl(rightSpeedSetpoint, rightSpeedError, rightSpeedErrorSum, prevRightSpeedError);
 
-  myPID.Compute();
+  // Use control values to set motor speeds
+  moveMotor(leftMotorPin1, leftMotorPin2, leftControl >= 0, leftControl < 0, leftMotorPinPWM, abs(leftControl));
+  moveMotor(rightMotorPin1, rightMotorPin2, rightControl >= 0, rightControl < 0, rightMotorPinPWM, abs(rightControl));
 
-  if(Output > 0){
-    digitalWrite(leftMotorPin1, HIGH);
-    digitalWrite(leftMotorPin2, LOW);
-    digitalWrite(rightMotorPin1, HIGH);
-    digitalWrite(rightMotorPin2, LOW);
-  } else {
-    digitalWrite(leftMotorPin1, LOW);
-    digitalWrite(leftMotorPin2, HIGH);
-    digitalWrite(rightMotorPin1, LOW);
-    digitalWrite(rightMotorPin2, HIGH);
-  }
-  
-  analogWrite(leftMotorPin1, abs(Output));
-  analogWrite(rightMotorPin1, abs(Output));
-  
-  Serial.print("Position: "); // Print the encoder value on the serial monitor
-  Serial.println((leftEncoderPos + rightEncoderPos) / 2.0);
+  // Compute odometry after performing motor actions
+  computeOdometry();
 
-  delay(10);
+  // Send position data over Serial
+  Serial.print("X: ");
+  Serial.print(xPosition);
+  Serial.print(" Y: ");
+  Serial.println(yPosition);
+}
+
+void moveMotor(int motorPin1, int motorPin2, int pin1State, int pin2State, int motorPinPWM, int speed) {
+  digitalWrite(motorPin1, pin1State);
+  digitalWrite(motorPin2, pin2State);
+  analogWrite(motorPinPWM, speed);
 }
 
 void updateLeftEncoder() {
-  int MSB = digitalRead(leftEncoderPinA); //MSB = most significant bit
-  int LSB = digitalRead(leftEncoderPinB); //LSB = least significant bit
-
-  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
-  int sum  = (leftEncoderPos << 2) | encoded; //adding it to the previous encoded value
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) leftEncoderPos ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) leftEncoderPos --;
+  updateEncoder(leftEncoderPinA, leftEncoderPinB, leftEncoderPos);
 }
 
 void updateRightEncoder() {
-  int MSB = digitalRead(rightEncoderPinA); //MSB = most significant bit
-  int LSB = digitalRead(rightEncoderPinB); //LSB = least significant bit
+  updateEncoder(rightEncoderPinA, rightEncoderPinB, rightEncoderPos);
+}
 
-  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
-  int sum  = (rightEncoderPos << 2) | encoded; //adding it to the previous encoded value
+void updateEncoder(int encoderPinA, int encoderPinB, volatile int &encoderPos) {
+  int MSB = digitalRead(encoderPinA);
+  int LSB = digitalRead(encoderPinB);
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) rightEncoderPos ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) rightEncoderPos --;
+  int encoded = (MSB << 1) | LSB;
+  int sum = (encoderPos << 2) | encoded;
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderPos++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderPos--;
+}
+
+void computeOdometry() {
+  // Compute the distance each wheel has moved
+  double leftDistance = (leftEncoderPos - prevLeftEncoderPos) * distancePerPulse;
+  double rightDistance = (rightEncoderPos - prevRightEncoderPos) * distancePerPulse;
+  
+  // Compute the change in position and angle
+  double distanceTravelled = (leftDistance + rightDistance) / 2;
+  double deltaTheta = (rightDistance - leftDistance) / wheelbase;
+  theta += deltaTheta;
+  
+  // Compute change in position
+  double deltaX = distanceTravelled * cos(theta);
+  double deltaY = distanceTravelled * sin(theta);
+  
+  // Update robot position
+  xPosition += deltaX;
+  yPosition += deltaY;
+  
+  // Update previous encoder positions
+  prevLeftEncoderPos = leftEncoderPos;
+  prevRightEncoderPos = rightEncoderPos;
+}
+
+double computePIDControl(double setpoint, double &error, double &errorSum, double &prevError) {
+  // Compute error
+  double currentError = setpoint - error;
+
+  // Compute integral and derivative terms
+  double integralTerm = errorSum;
+  double derivativeTerm = currentError - prevError;
+
+  // Compute control value
+  double control = Kp * currentError + Ki * integralTerm + Kd * derivativeTerm;
+
+  // Update error values
+  errorSum += currentError;
+  prevError = currentError;
+
+  return control;
 }
