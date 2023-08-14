7195161238bb1250b03af0e1c7f76f9d12eaae76
Jaeyeong Hwang <99489807+jwyeeh-dev@users.noreply.github.com>
2023-08-15 01:16:17 +0900

    update 1st PID Gain decision

--- ControlParts/ArduinoPart/anglevelPID.ino
+++ ControlParts/ArduinoPart/anglevelPID.ino
@@ -5,17 +5,19 @@
 #define encoder1PinB 8
 #define leftMotorPin1 11
 #define leftMotorPin2 10
-#define rightMotorPin1 5
-#define rightMotorPin2 6
+#define rightMotorPin1 6
+#define rightMotorPin2 5
 #define leftMotorPinPWM 12
 #define rightMotorPinPWM 7
 
 // define absolute values which we use
-#define P_GAIN 15.0
-#define I_GAIN 35.0
-#define D_GAIN 20.0
-#define TARGET_SPEED 0.7
+#define P_GAIN 30.0
+#define I_GAIN 20.0
+#define D_GAIN 10.0
 
+//values which related by cmd_topic
+const double TARGET_SPEED = 0.7; //m/s
+const double TARGET_ANGLESPEED =  90/180/3.14; //radian
 
 // define robot specification values
 const double gearboxRatio = 27.0;
@@ -24,7 +26,11 @@ const double wheelDiameter = 0.13;  // meter
 const double wheelbase = 0.30;      // meter
 const double wheelCircumference = wheelDiameter * PI;
 const double distancePerPulse = wheelCircumference / (encoderPulsesPerRevolution * gearboxRatio);
+float speedDifference = TARGET_ANGLESPEED * wheelbase;  // 바퀴의 속도 차이 계산
 
+// 각 바퀴의 목표 속도 설정
+float targetSpeed0 = TARGET_SPEED + speedDifference / 2;
+float targetSpeed1 = TARGET_SPEED - speedDifference / 2;
 
 // define related values in code
 float currentSpeed0 = 0.00;
@@ -54,6 +60,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(encoder1PinA), updateEncoder1, CHANGE);
 
   Serial.begin(115200);
+
 }
 
 // loop function
@@ -61,42 +68,42 @@ void loop() {
   unsigned long currentTime = millis();
   float dt = (currentTime - lastUpdateTime) / 1000.0;  // Convert to seconds
 
-  if (currentTime - lastUpdateTime >= 50) {
-    currentSpeed0 = (encoder0Pos - encoder0PosLast) * distancePerPulse * 20;  // 50ms 동안의 이동거리를 초당 속도로 변환
-    currentSpeed1 = (encoder1Pos - encoder1PosLast) * distancePerPulse * 20;
+  if (currentTime - lastUpdateTime >= 20) {
+    currentSpeed0 = (encoder0Pos - encoder0PosLast) * distancePerPulse * 50;  // 50ms 동안의 이동거리를 초당 속도로 변환
+    currentSpeed1 = (encoder1Pos - encoder1PosLast) * distancePerPulse * 50;
+    
     encoder0PosLast = encoder0Pos;
     encoder1PosLast = encoder1Pos;
     lastUpdateTime = currentTime;
   }
 
-  if(encoder0Pos < 25000 || encoder1Pos < 25000){
-    calculateError(&error0, &accError0, &errorGap0, currentSpeed0, TARGET_SPEED);
-    float pidControl0 = pidControlSystem(error0, accError0, errorGap0, dt);
-    moveMotor(leftMotorPinPWM, leftMotorPin1, leftMotorPin2, pidControl0);
+  calculateError(&error0, &accError0, &errorGap0, currentSpeed0, TARGET_SPEED);
+  float pidControl0 = pidControlSystem(error0, accError0, errorGap0, dt);
+  moveMotor(leftMotorPinPWM, leftMotorPin1, leftMotorPin2, pidControl0);
 
-    calculateError(&error1, &accError1, &errorGap1, currentSpeed1, TARGET_SPEED);
-    float pidControl1 = pidControlSystem(error1, accError1, errorGap1, dt);
-    moveMotor(rightMotorPinPWM, rightMotorPin1, rightMotorPin2, pidControl1);
-
-  }
-  else {
-    moveMotor(leftMotorPinPWM, leftMotorPin1, leftMotorPin2, 0);
-    moveMotor(rightMotorPinPWM, rightMotorPin1, rightMotorPin2, 0);
-  }
+  calculateError(&error1, &accError1, &errorGap1, currentSpeed1, TARGET_SPEED);
+  float pidControl1 = pidControlSystem(error1, accError1, errorGap1, dt);
+  moveMotor(rightMotorPinPWM, rightMotorPin1, rightMotorPin2, pidControl1);
 
   Serial.print(TARGET_SPEED);
-  Serial.print("\t");
+  Serial.print(",");
+  Serial.print(TARGET_ANGLESPEED);
+  Serial.print(",");
   Serial.print(currentSpeed0);
-  Serial.print("\t");
+  Serial.print(",");
   Serial.print(currentSpeed1);
-  Serial.print("\t");
+  Serial.print(",");
   Serial.print(error0);
-  Serial.print("\t");
+  Serial.print(",");
   Serial.print(error1);
-  Serial.print("\t");
-  Serial.print(encoder0Pos);
-  Serial.print("\t");
-  Serial.println(encoder1Pos);
+  Serial.print(",");
+  Serial.print(pidControl0);
+//  Serial.print(",");
+//  Serial.print(pidControl1);
+  Serial.print(",");
+  Serial.print(constrain(abs(pidControl0), 0, 255));
+  Serial.print(",");
+  Serial.println(constrain(abs(pidControl1), 0, 255));
 }
 
 // 2체배 (left)
@@ -127,9 +134,9 @@ void calculateError(float* error, float* accError, float* errorGap, float curren
 // PID Control Function
 float pidControlSystem(float error, float accError, float errorGap, float dt) {
   float pControl = P_GAIN * error;
-  float iControl = I_GAIN * (accError * dt);
+  float iControl = I_GAIN * (accError);
   float dControl = D_GAIN * (errorGap / dt);
-  return abs(pControl + iControl + dControl);
+  return pControl + iControl + dControl;
 }
 
 // Moving Motor by PWM constant which is regulated 
